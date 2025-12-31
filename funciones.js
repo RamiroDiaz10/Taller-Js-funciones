27621e812ab69a9b9bda932993323ffc9e3f60eb
Ramiro Diaz <jramiroda10@gmail.com>
2025-12-31 00:00:32 -0500

    [+] funciones de la 6 hasta la 12

--- funciones.js
+++ funciones.js
@@ -37,6 +37,53 @@ function buscarElemento (array, elemento){
     const element = array.includes(elemento);
     return element;
 }
+//06. calcular promedio de notas
+function promedioNotas (estudiante) {
+    let nota = estudiante.notas;
+    let promedio = 0;
+    nota.forEach( num => promedio +=num / nota.length );
+    return promedio;
+}
+//07. filtrar estudiantes aprovados
+function aprobados (listaEstudiantes){
+    let resultado = listaEstudiantes.filter( note => note.notas >= 60);
+    return resultado;
+}
+//08. Agregar propiedad a objeto
+function agregarPropiedad (objeto, clave, valor){
+    objeto.password = clave;
+    objeto.altura = valor;
+    
+    return objeto;
+}
+//09. Combinar dos objetos
+function combinarObjetos(object1, object2){
+    const combi = { ...object1, ...object2};
+    return combi;
+} 
+//10.contar propiedades de un objeto
+function contarPropiedades (objeto){
+    const total = Object.keys(objeto).length;
+    return total;
+}
+//11. calcular area y perimetro de un rectangulo
+function calcularRectangulo ( ancho, alto ){
+    const perimetro = (ancho + alto ) *2;
+    const area = ancho * alto;
+    const total = { area: area, perimetro: perimetro };
+    return total;
+}
+//12. Buscar el estudiante con la nota mas alta
+function mejorEstudiante (array){
+    return array.reduce((mayor, actual) =>{
+        return actual.nota > mayor.nota ? actual : mayor
+    });
+    
+} 
+//13. eliminar elementos duplicados de un array
+function eliminarDuplicados (array) {
+
+}
 
 
 export {
@@ -45,4 +92,12 @@ export {
     contarPares,
     invertirArray,
     buscarElemento,
-}
+    promedioNotas,
+    aprobados,
+    agregarPropiedad,
+    combinarObjetos,
+    contarPropiedades,
+    calcularRectangulo,
+    mejorEstudiante,
+    eliminarDuplicados,
+} 
--- resultados.js
+++ resultados.js
@@ -1,4 +1,4 @@
-import { buscarElemento, contarPares, invertirArray, numeroMayor, sumarArray } from "./funciones.js";
+import { agregarPropiedad, aprobados, buscarElemento, calcularRectangulo, combinarObjetos, contarPares, contarPropiedades, invertirArray, mejorEstudiante, numeroMayor, promedioNotas, sumarArray } from "./funciones.js";
 
 
 const sumNumbers = [1,2,3,4,5,6,7];                //01
@@ -6,6 +6,44 @@ const numMayor = [1,23,34,5,6,7,46];               //02
 const numberPar = [2,3,4,5,6,7,8,24,25,36,10];     //03
 const invertArray = ['a','b','c','d'];             //04
 const searchElement = ['arroz','papa','verduras','frutas'];  //05
+const studentNotes = { nombre: 'Cesar', notas: [ 100, 90, 90 ]} //06
+const students = [                                              //07
+    { nombre: 'Andres', notas: [50]},
+    { nombre: 'Flor', notas: [60]},
+    { nombre: 'Martin', notas: [40]},
+    { nombre: 'Jorge', notas: [90]},
+    { nombre: 'Robert', notas: [100]}
+]
+const people = { nombre: 'Andres', Edad: 26, }                  //08
+//..............................................................//09
+const oneObject = { 
+    name: 'camilo', 
+    Edad: 33
+}
+const twoObject = { 
+    password: '12345', 
+    altura: 65
+
+}
+//..............................................................
+const dates = {                                                 //10
+    name: 'robert',
+    edad: 34,
+    password: '12345'
+}
+//..............................................................//11
+const broad = 23;
+const high = 32;
+//..............................................................//12
+const student = [
+    {name: 'joshep', nota: 60},
+    {name: 'luis', nota: 10},
+    {name: 'juan', nota: 20},
+    {name: 'omar', nota: 90}
+]
+
+
+    
 
 
 
@@ -14,3 +52,10 @@ console.log(numeroMayor(numMayor));
 console.log(contarPares(numberPar));
 console.log(invertirArray(invertArray));
 console.log(buscarElemento(searchElement,'papa'));
+console.log(promedioNotas(studentNotes));
+console.log(aprobados(students));
+console.log(agregarPropiedad(people, '12345', 24));
+console.log(combinarObjetos(oneObject, twoObject));
+console.log(contarPropiedades(dates));
+console.log(calcularRectangulo( broad, high ))
+console.log(mejorEstudiante(student));
